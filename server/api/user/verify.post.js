205dbb3affc2d4e5e74d0453321db0d457d92083
Chloie Tan <chloie.tan.ct@gmail.com>
2025-02-15 17:11:49 +0800

    Add verify user API

---
+++ server/api/user/verify.post.js
@@ -0,0 +1,18 @@
+import prisma from '~/lib/prisma';
+
+export default defineEventHandler(async (event) => {
+  const { username, password } = await readBody(event);
+
+  return await prisma.user.findFirst({
+    where: {
+      AND: [{ username: username }, { password: password }],
+    },
+    include: {
+      UserAvatar: {
+        where: {
+          selected: true,
+        },
+      },
+    },
+  });
+});
