1dfb3fa775fc7591e66834504c5d06eb0dd07e07
Chloie Tan <chloie.tan.ct@gmail.com>
2025-02-15 15:33:21 +0800

    Add test page routing

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
-<script setup lang="ts">
-  const { data } = await useFetch('/api/hello')
-</script>
-
 <template>
-  <pre>{{ data }}</pre>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+const { data } = await useFetch('/api/hello');
+</script>
+
+<template>
+  <pre>{{ data }}</pre>
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
+</template>
